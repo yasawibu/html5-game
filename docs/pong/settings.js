41f4a10cd7bbc0cc6e7fa4aed8f41688a8e61d3a
Putu Ardi Dharmayasa <36402967+yasawibu@users.noreply.github.com>
2018-09-05 21:29:45 +0700

    Create settings.js

---
+++ docs/pong/settings.js
@@ -0,0 +1,16 @@
+const GAME_FPS = 60;
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 500;
+const GAME_BACKGROUND_COLOR = 'black';
+
+const BALL_SPEED_MIN = 5;
+const BALL_SPEED_MAX = 15;
+const BALL_RADIUS = 10;
+const BALL_FILL_COLOR = 'red';
+const BALL_LINE_COLOR = 'black';
+
+const PADDLE_MARGIN = 40;
+const PADDLE_WIDTH = 20;
+const PADDLE_HEIGHT = 80;
+const PADDLE_FILL_COLOR = 'white';
+const PADDLE_LINE_COLOR = 'red';
